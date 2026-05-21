e882643ed1cae9e541dab119b7f7fba04cdace0f
Varun Kumar <Varun251005@users.noreply.github.com>
2026-05-21 16:09:28 +0530

    Update Procfile and asgi.py for improved server configuration and security

--- backend/chat_backend/asgi.py
+++ backend/chat_backend/asgi.py
@@ -2,6 +2,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 import chat_backend.routing
@@ -13,8 +14,8 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(
-            URLRouter(chat_backend.routing.websocket_urlpatterns)
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat_backend.routing.websocket_urlpatterns))
         ),
     }
 )
