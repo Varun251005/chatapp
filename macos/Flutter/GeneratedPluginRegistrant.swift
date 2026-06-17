1cd7ccc7cf7d8b7c4d3e09450713575f37679e8d
Varun Kumar <Varun251005@users.noreply.github.com>
2026-06-17 17:10:51 +0530

    Register MobileScannerPlugin in GeneratedPluginRegistrant for enhanced scanning functionality

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_webrtc
+import mobile_scanner
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
 }
