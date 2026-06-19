ae195fa6f3741af3e28ecf39fb0576945ad62391
Varun Kumar <Varun251005@users.noreply.github.com>
2026-06-19 22:43:24 +0530

    making changes in app router file

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -3,18 +3,23 @@ import 'package:go_router/go_router.dart';
 import '../../features/room/screens/chat_room_screen.dart';
 import '../../features/room/screens/create_room_screen.dart';
 import '../../features/room/screens/home_screen.dart';
+import '../../features/room/screens/join_room_screen.dart';
 import '../../features/room/screens/room_share_screen.dart';
 import '../../features/room/screens/splash_screen.dart';
+import '../../features/room/screens/video_call_screen.dart';
 
 GoRouter buildAppRouter() {
   return GoRouter(
-    initialLocation: '/splash',
+    initialLocation: '/home',
     routes: <RouteBase>[
+      GoRoute(path: '/', redirect: (context, state) => '/home'),
       GoRoute(path: '/splash', builder: (context, state) => const SplashScreen()),
       GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
       GoRoute(path: '/create', builder: (context, state) => const CreateRoomScreen()),
+      GoRoute(path: '/join', builder: (context, state) => const JoinRoomScreen()),
       GoRoute(path: '/share', builder: (context, state) => const RoomShareScreen()),
       GoRoute(path: '/chat', builder: (context, state) => const ChatRoomScreen()),
+      GoRoute(path: '/video', builder: (context, state) => const VideoCallScreen()),
     ],
   );
 }
